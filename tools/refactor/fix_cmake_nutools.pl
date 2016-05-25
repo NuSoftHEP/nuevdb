232402c1c4fe5e4c98b256a4c0f107c66e5a9f20
Lynn Garren <garren@fnal.gov>
2016-05-25 15:25:06 -0500

    translate library names

--- tools/refactor/fix_cmake_nutools.pl
+++ tools/refactor/fix_cmake_nutools.pl
@@ -2,14 +2,34 @@ use strict;
 
 use vars qw(%dir_list);
 BEGIN { %dir_list = (
-	"SimulationBase" => "nusimdata_SimulationBase",
+	"Colors_service" => "nutools_Colors_service",
+	"DBI_service" => "nutools_",
+	"EventDisplayBase" => "nutools_EventDisplayBase",
+	"EventDisplay_service" => "nutools_EventDisplay_service",
+	"EventGeneratorBaseCRY" => "nutools_EventGeneratorBase_CRY",
+	"EventGeneratorBaseGENIE" => "nutools_EventGeneratorBase_GENIE",
+	"EventGeneratorBaseGiBUU" => "nutools_EventGeneratorBase_GiBUU",
+	"G4Base" => "nutools_G4Base",
+	"IFDatabase" => "nutools_IFDatabase",
+	"MagneticField_service" => "nutools_MagneticField_service",
+	"NuBeamWeights" => "nutools_NuBeamWeights",
+	"NuReweightArt" => "nutools_NuReweightArt",
+	"NuReweight" => "nutools_NuReweight",
+	"ReweightAna_module" => "nutools_ReweightAna_module",
+	"ScanOptions_service" => "nutools_ScanOptions_service",
+	"SimulationBase_dict" => "nutools_SimulationBase_dict",
+	"SimulationBase" => "nusimdata_SimulationBase"
 		       ); }
 
 foreach my $lib (sort keys %dir_list) {
    next if m&add_subdirectory&i;
    next if m&simple_plugin&i;
+   next if m&SUBDIRNAME&i;
+   next if m&SUBDIRS&i;
+   next if m&PACKAGE&i;
   #s&\b\Q${lib}\E([^\.\s]*\b)([^\.]|$)&$dir_list{$lib}${1}${2}&g and last;
   s&\b\Q${lib}\E\b([^\.]|$)&$dir_list{$lib}${1}${2}&g and last;
 }
 
-s&\$\{SIMULATIONBASE\}&nusimdata_SimulationBase&g;
+#s&\$\{SIMULATIONBASE\}&nusimdata_SimulationBase&g;
+#	"EventGeneratorBase_test_EventGeneratorTest_module" => "nutools_",
